8e45eeb54fa13745aa40e7ef22de2124a2ae36f1
Igor Melnikov <igor.melnikov@esrf.fr>
2023-12-07 14:54:17 +0100

    07122023

--- dozorm_colormap.py
+++ dozorm_colormap.py
@@ -12,54 +12,54 @@ from matplotlib import colors, pyplot as plt
 from matplotlib.patches import Circle as C
 from scipy import ndimage, signal
 
-#def parser(filename):
-#    l = []
-#    with open(filename, 'r') as f:
-#        l = f.readlines()
-#        f.close()
-#    
-##    print(re.split(r'\s+', l[1])[1:3])
-#    col, row = [int(x) for x in re.split(r'\s+', l[1])[1:3]]
-#    
-##    Dtable
-#    Dtable = numpy.zeros((row, col))
-#    i = -4
-#    for line in l:
-#        if re.search('Map of Scores', line):
-#            i = -3
-#        
-#        if i>=0:
-##            print(numpy.array(re.findall('.{6}', line[5:])).astype(float).size)
-#            Dtable[i, :] = numpy.array(re.findall('.{6}', line[5:])).astype(float)
-#
-#        if i>=row-1:
-#            break
-#
-#        if i>=-3:
-#            i += 1
-#    
-##    Ztable
-#    Ztable = numpy.zeros((row, col))
-#    i = -4
-#    for line in l:
-#        if re.search('Map of Crystals', line):
-#            i = -3
-#        
-#        if i>=0:
-##            print(numpy.array(re.findall('.{4}', line[5:])).astype(int).size)
-#            Ztable[i, :] = numpy.array(re.findall('.{4}', line[5:])).astype(int)
-#
-#        if i>=row-1:
-#            break
-#
-#        if i>=-3:
-#            i += 1
-#
-#    
-##    plt.imshow(Dtable)
-##    plt.show()
-#
-#    return Dtable, Ztable
+def parser(filename):
+    l = []
+    with open(filename, 'r') as f:
+        l = f.readlines()
+        f.close()
+    
+#    print(re.split(r'\s+', l[1])[1:3])
+    col, row = [int(x) for x in re.split(r'\s+', l[1])[1:3]]
+    
+#    Dtable
+    Dtable = numpy.zeros((row, col))
+    i = -4
+    for line in l:
+        if re.search('Map of Scores', line):
+            i = -3
+        
+        if i>=0:
+#            print(numpy.array(re.findall('.{6}', line[5:])).astype(float).size)
+            Dtable[i, :] = numpy.array(re.findall('.{6}', line[5:])).astype(float)
+
+        if i>=row-1:
+            break
+
+        if i>=-3:
+            i += 1
+    
+#    Ztable
+    Ztable = numpy.zeros((row, col))
+    i = -4
+    for line in l:
+        if re.search('Map of Crystals', line):
+            i = -3
+        
+        if i>=0:
+#            print(numpy.array(re.findall('.{4}', line[5:])).astype(int).size)
+            Ztable[i, :] = numpy.array(re.findall('.{4}', line[5:])).astype(int)
+
+        if i>=row-1:
+            break
+
+        if i>=-3:
+            i += 1
+
+    
+#    plt.imshow(Dtable)
+#    plt.show()
+
+    return Dtable, Ztable
 #
 #parser('/data/id23eh1/inhouse/opid231/20230801/PROCESSED_DATA/DOZORM2_TESTMB/1/MeshScan_01/Workflow_20230801-114054/DozorM2_mesh-mb_1_01/dozorm_001.map')
 
@@ -123,13 +123,14 @@ def MainPlot(crystalN_array, dozorscore_array):
 #    print(numpy.sort(numpy.unique(Ztable[Ztable>0])))
     
     clrs = ConstructColorlist(Ztable)
-
     cmap = colors.ListedColormap([colors.to_rgba(str(i)) for i in clrs])
+    bounds = numpy.arange(-2, int(numpy.max(Ztable))+1)
+    norm = colors.BoundaryNorm(bounds, numpy.unique(clrs).size)
     if numpy.max(Ztable>0):
-        plt.imshow(Ztable, cmap=cmap, interpolation='nearest', origin='upper', \
+        plt.imshow(Ztable, cmap=cmap, norm=norm, interpolation='nearest', origin='upper', \
                    extent=[0.5, (col + 0.5), (row + 0.5), 0.5])
-            
-        m = int(round(numpy.log10(numpy.max(Dtable))-2, 0))
+
+        m = int(numpy.log10(numpy.max(Dtable)))
         M = numpy.max(Dtable)
         for (j, i) in numpy.ndindex((row, col)):
             if Ztable[j, i] > 0:
@@ -156,8 +157,8 @@ def MainPlot(crystalN_array, dozorscore_array):
                 ax.add_patch(C((i+1, j+1), Dtable[j, i]/(3*M), color='white'))
     
         plt.text(col+1, 2, 'Dozor\nscore', c='black', ha='left', va='center', size=10)
-        ax.add_patch(C((col+1.5, 4), 0.333, color='black', clip_on=False))
-        plt.text(col+2, 4, ('{:.'+str(m)+'f}').format(M), c='black', ha='left', va='center', size=10)
+        ax.add_patch(C((col+1.5, 4), 0.333*round(M, -m+1)/M, color='black', clip_on=False))
+        plt.text(col+2, 4, ('{:.'+('0' if m>0 else '1')+'f}').format(round(M, -m+1)), c='black', ha='left', va='center', size=10)
     else:
         plt.imshow(Dtable, cmap='hot', interpolation='nearest', origin='upper', \
                    extent=[0.5, (col + 0.5), (row + 0.5), 0.5])
@@ -175,8 +176,9 @@ def MainPlot(crystalN_array, dozorscore_array):
     
 #D, Z = parser('/data/id23eh1/inhouse/opid231/20230801/PROCESSED_DATA/DOZORM2_TESTMB/1/MeshScan_01/Workflow_20230801-114054/DozorM2_mesh-mb_1_01/dozorm_001.map')
 #D, Z = parser('/opt/pxsoft/mxworkflows/id30a2/edna2/tests/test_tasks/DozorM2/data/dozorm_001.map')
-#
-#MainPlot(Z, D)
+D, Z = parser('/home/esrf/melnikov/spyder/test/dozorm_001.map')
+
+MainPlot(Z, D)
 #
 #
 #for item in glob.glob('dozor*.map'):
